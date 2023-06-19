ee8518da131302e9d139b89273ea4dd53f3bce7e
Elmira Faizova <arimgiz@yandex.ru>
2023-06-19 20:00:46 +0300

    скрытие футера

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function Footer() {
+  const location = useLocation();
+  const hideCopyRight =
+    location.pathname === "/sign-up" || location.pathname === "/sign-in";
+
   return (
     <footer className="footer">
-      <p className="footer__copyright">&copy;2023 Mesto Russia</p>
+      {!hideCopyRight && (
+        <p className="footer__copyright">&copy;2023 Mesto Russia</p>
+      )}
     </footer>
   );
 }
